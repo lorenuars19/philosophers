615991294bf4652c6be025fe263275f02728b81c
lorenuar.s19 <lorenuar.s19@gmail.com>
2021-09-01 11:33:17 +0200

    Base of the code

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: lorenuar <lorenuar@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/23 16:26:02 by lorenuar          #+#    #+#             */
-/*   Updated: 2021/08/23 17:06:26 by lorenuar         ###   ########.fr       */
+/*   Updated: 2021/09/01 10:50:47 by lorenuar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,21 +20,34 @@
 # include <sys/time.h>
 # include <pthread.h>
 
-# define THREADS_MAX 42
+# define THREADS_MAX 256
 
-typedef struct s_philosopher_data
+typedef enum e_fork_state
 {
-	pthread_t		threads[THREADS_MAX];
-	int				returns[THREADS_MAX];
-	pthread_mutex_t	mutex;
-}	t_pdat;
+	STATE_DEADLOCKED = -1,
+	STATE_AVAILABLE = 0,
+}	t_fork;
 
-typedef struct s_thread_data
+typedef unsigned long	t_time;
+typedef struct s_data
 {
-	int				num;
-	pthread_mutex_t	*mutex_ptr;
-	int				counter;
-}	t_tdat;
+	long		n_philo;
+	t_time		time_die;
+	t_time		time_eat;
+	t_time		time_sleep;
+	long		max_meals;
 
+	pthread_t	thread_ids[THREADS_MAX];
+
+	t_time		time_until_death[THREADS_MAX];
+
+	t_fork		forks[THREADS_MAX];
+}	t_data;
+
+/* debug */
+void	print_data(t_data *dat);
+
+
+ssize_t	str_to_nbr(const char *s);
 
 #endif
