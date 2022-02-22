6c4ba842e463811b83e2c21791a27826a163a014
lorenuar.s19 <lorenuar.s19@gmail.com>
2022-02-22 18:01:03 +0100

    refactor kill_and_destroy()

--- philo/src/kill_and_destroy.c
+++ philo/src/kill_and_destroy.c
@@ -6,7 +6,7 @@
 /*   By: lorenuar <lorenuar@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/06 12:11:05 by lorenuar          #+#    #+#             */
-/*   Updated: 2022/02/22 16:23:34 by lorenuar         ###   ########.fr       */
+/*   Updated: 2022/02/22 17:26:00 by lorenuar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,14 @@ static int	kill_threads(t_data *dat)
 	int	i;
 
 	i = 0;
+	usleep(((dat->time_sleep) * 1.1) * 1000);
 	while (i < dat->n_philo)
 	{
-		if (dat->threads[i] && pthread_join(dat->threads[i], NULL))
+		if (dat->threads[i] && pthread_detach(dat->threads[i]))
 		{
 			return (1);
 		}
 		dat->threads[i] = (pthread_t) NULL;
-PDAT(kill_threads, dat)
 		i++;
 	}
 	return (0);
