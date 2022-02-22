57841644bf4dcc10efb073e63385f7e2b07dd08d
lorenuar.s19 <lorenuar.s19@gmail.com>
2022-02-22 16:07:02 +0100

    refactor check+philo_death()

--- philo/src/check_philo_death.c
+++ philo/src/check_philo_death.c
@@ -6,23 +6,23 @@
 /*   By: lorenuar <lorenuar@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/07 14:42:51 by lorenuar          #+#    #+#             */
-/*   Updated: 2022/02/07 13:58:53 by lorenuar         ###   ########.fr       */
+/*   Updated: 2022/02/22 15:52:44 by lorenuar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-static int	time_check_death(t_data *dat, t_time philo_time, long philo_id)
+static int	time_check_death(t_data *dat, t_time philo_time)
 {
 	t_time	now;
 
 	if (time_get_now(&now))
 	{
-		return (1);
+		//TODO handle error
 	}
 	if ((now >= (philo_time + dat->time_die)))
 	{
-		dat->state[philo_id] = STATE_DEAD;
+		return (1);
 	}
 	return (0);
 }
@@ -31,36 +31,39 @@ int	check_philo_death(t_data *dat)
 {
 	int i;
 	int	x;
+	int	check_death;
+	int	ret;
 
 	x = 0;
+	ret = 0;
 	if (mutex_lock(&(dat->mutex_data)))
 	{
-		return (1);
+		return (-1);
 	}
 	while (x < dat->n_philo)
 	{
-		if (time_check_death(dat, dat->time_last_meal[x], x))
+		check_death = time_check_death(dat, dat->time_last_meal[x]);
+		if (check_death < 0)
 		{
-			break;
+			return (-1);
 		}
-		if (dat->state[x] == STATE_DEAD)
+		if (check_death == 1)
 		{
-			if (dat->philo_death == NOBODY_DEAD)
-			{
-				dat->philo_death = x;
-			}
+			ret = x + 1;
+			dat->state[x] = STATE_DEAD;
 			i = 0;
 			while (i < THREADS_MAX)
 			{
 				dat->state[i] = STATE_DEAD;
 				i++;
 			}
+			break ;
 		}
 		x++;
 	}
 	if (mutex_unlock(&(dat->mutex_data)))
 	{
-		return (1);
+		return (-1);
 	}
-	return (0);
+	return (ret);
 }
