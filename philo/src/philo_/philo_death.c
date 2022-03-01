f458dfdb9a64de5a45b058f3a9dde1f2a520bf8c
lorenuars19 <lorenuar.s19@gmail.com>
2022-03-01 14:42:55 +0100

    fix messages being printed after dead

--- philo/src/philo_/philo_death.c
+++ philo/src/philo_/philo_death.c
@@ -6,7 +6,7 @@
 /*   By: lorenuar <lorenuar@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/06 11:19:49 by lorenuar          #+#    #+#             */
-/*   Updated: 2022/03/01 14:13:30 by lorenuar         ###   ########.fr       */
+/*   Updated: 2022/03/01 14:35:30 by lorenuar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,11 @@ void	philo_death(t_phil_dat *pdat, t_data *dat)
 	mutex_lock(&(dat->mutex_data));
 	check_death = time_check_death(dat, dat->time_last_meal[pdat->id]);
 	state = dat->state[pdat->id];
-	mutex_unlock(&(dat->mutex_data));
 	if (check_death || state == STATE_DEAD)
 	{
-		dat_set_state(dat, pdat->id, STATE_DEAD);
-		usleep(50 * 10000);
+		dat->state[pdat->id] = STATE_DEAD;
+		mutex_unlock(&(dat->mutex_data));
 		pthread_exit(NULL);
 	}
+	mutex_unlock(&(dat->mutex_data));
 }
