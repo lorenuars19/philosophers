d43b40aa9cbbc894d8e403c47f5fe784bd4d70da
lorenuars19 <lorenuar.s19@gmail.com>
2022-03-07 16:09:07 +0100

    suce mon git

---
+++ src/main.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lorenuar <lorenuar@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/01 17:32:08 by lorenuar          #+#    #+#             */
+/*   Updated: 2022/03/07 14:59:34 by lorenuar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+static int	print_usage(char *msg, char *prog)
+{
+	printf("\033[31;1m%s\033[0m\n=== Usage ===\n"
+		" %s [number_of_philo] [time_to_die(ms)]"
+		" [time_to_eat(ms)] [time_to_sleep(ms)]"
+		" [maximum_number_of_meals (optional)] \n=============\n",
+		msg, prog);
+	return (1);
+}
+
+int	main(int argc, char *argv[])
+{
+	t_data	dat;
+
+	if (!(argc == 5 || argc == 6))
+	{
+		print_usage("Invalid number of arguments", argv[0]);
+		return (1);
+	}
+	if (init_data(&dat, argc, argv))
+	{
+		print_usage("Error from init_data()", argv[0]);
+		return (1);
+	}
+	if (spawn_threads(&dat))
+	{
+		return (1);
+	}
+	if (kill_and_destroy(&dat))
+	{
+		return (1);
+	}
+	return (0);
+}
