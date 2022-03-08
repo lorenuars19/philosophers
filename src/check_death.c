5ebd1dc47221da975c92a53cd90ad4ce8a164c1c
lorenuars19 <lorenuar.s19@gmail.com>
2022-03-08 12:18:25 +0100

    TODO

--- src/check_death.c
+++ src/check_death.c
@@ -6,7 +6,7 @@
 /*   By: lorenuar <lorenuar@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/07 09:08:43 by lorenuar          #+#    #+#             */
-/*   Updated: 2022/03/08 11:15:40 by lorenuar         ###   ########.fr       */
+/*   Updated: 2022/03/08 12:06:16 by lorenuar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,9 +26,9 @@ void	check_death(t_data *dat)
 			if (dat->phi_arr[i].last_meal != 0 && (get_time_ms()
 					>= (dat->phi_arr[i].last_meal + dat->time_die)))
 			{
-				dead = 1;
 				if (dat->max_meals == 0)
 				{
+					dead = 1;
 					print_timed_msg(&(dat->phi_arr[i]), "is dead");
 					pthread_mutex_lock(&(dat->mutex_print));
 					return ;
